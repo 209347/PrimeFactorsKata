8b4126a840650d6c95dfb2f0ad0afd6123663d12
209347 <209347@edu.p.lodz.pl>
2019-06-26 21:30:00 +0200

    test6 green

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -5,11 +5,13 @@ import java.util.List;
 public class PrimeFactors {
     public static List<Integer> of(int value) {
         List<Integer> factors = new ArrayList<>();
-        if (value > 1) {
-            while (value % 2 == 0) {
-                factors.add(2);
-                value /= 2;
+        int divisor = 2;
+        while (value > 1) {
+            while (value % divisor == 0) {
+                factors.add(divisor);
+                value /= divisor;
             }
+            divisor++;
         }
         if(value > 1) {
             factors.add(value);
