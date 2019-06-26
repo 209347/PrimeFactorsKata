8319442393cd2684522aa128a8379eb1daf98433
209347 <209347@edu.p.lodz.pl>
2019-06-26 21:13:19 +0200

    test2 green

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,8 +1,13 @@
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static List<Integer> of(int value) {
-        return new ArrayList<Integer>();
+        List<Integer> factors = new ArrayList<>();
+        if(value == 2) {
+            factors.add(2);
+        }
+        return factors;
     }
 }
