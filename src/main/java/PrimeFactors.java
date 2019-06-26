eaaf38a14bd8808c23f52cddfe81854ef736672a
209347 <209347@edu.p.lodz.pl>
2019-06-26 21:16:37 +0200

    test3 green

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class PrimeFactors {
     public static List<Integer> of(int value) {
         List<Integer> factors = new ArrayList<>();
-        if(value == 2) {
-            factors.add(2);
+        if(value > 1) {
+            factors.add(value);
         }
         return factors;
     }
