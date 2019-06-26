390e5bd8a385a0a2bd74720595d765325452ade6
209347 <209347@edu.p.lodz.pl>
2019-06-26 20:21:14 +0200

    test0 - itCompiles

---
+++ src/test/java/PrimeFactorsTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class PrimeFactorsTest {
+
+    @Test
+    public void itCompiles() {
+        assertThat(true, equalTo(true));
+    }
+
+}
